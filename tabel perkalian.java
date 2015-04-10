41699f5d6b841792d48b1dd7516572d0630c1ccb
januarrosydi <14410100053@stikom.edu>
2015-04-10 23:59:45 +0700

    membuat tabel perkalian
    memahami model perulangan for.

---
+++ tabel perkalian.java
@@ -0,0 +1,16 @@
+public class tabelPerkalian{
+public static void main (String[]yanuar){
+
+  
+  
+  for(int a=1;a<=9;){
+    for(int b=1;b<=9;){
+     int c=a*b;
+     System.out.print(a+"x"+b+"="+c+"\t");
+     b++;
+    }
+    System.out.println(" ");
+    a++;
+  }
+}
+} 
